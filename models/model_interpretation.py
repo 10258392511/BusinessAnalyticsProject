5df611ba17a79e02cb2c8bca154b5ab17a6b7160
Xinran Cheng <121287133+Cheshire4@users.noreply.github.com>
2024-03-17 21:00:26 +0100

    Add files via upload

---
+++ models/model_interpretation.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from statsmodels.regression.linear_model import RegressionResults
+from sklearn.pipeline import Pipeline
+
+
+def create_benchmark_plot(results: RegressionResults, conf_level=0.05, **kwargs):
+    """
+    kwargs: figsize
+    """
+    figsize = kwargs.get("figsize", (18, 7.2))
+
+    p_vals = results.pvalues
+    sig_mask = p_vals <= conf_level
+    sig_features = p_vals[sig_mask].sort_values()
+    insig_features = p_vals[~sig_mask].sort_values(ascending=False)
+
+    fig, axes = plt.subplots(2, 1, figsize=figsize)
+    sig_features.plot(kind="bar", rot=90, ax=axes[0], title="Significant Features", ylabel="p-value")
+    insig_features.plot(kind="bar", rot=90, ax=axes[1], title="Insignificant Features", ylabel="p-value")
+
+    fig.tight_layout()
+
+    return fig, axes
+
+def create_benchmark_plot_with_p(pval, results: RegressionResults, conf_level=0.05, **kwargs):
+    """
+    kwargs: figsize
+    """
+    figsize = kwargs.get("figsize", (18, 7.2))
+
+    p_vals = pd.Series(pval)
+    sig_mask = p_vals <= conf_level
+    sig_features = p_vals[sig_mask].sort_values()
+    insig_features = p_vals[~sig_mask].sort_values(ascending=False)
+
+    fig, axes = plt.subplots(2, 1, figsize=figsize)
+    sig_features.plot(kind="bar", rot=90, ax=axes[0], title="Significant Features", ylabel="p-value")
+    insig_features.plot(kind="bar", rot=90, ax=axes[1], title="Inignificant Features", ylabel="p-value")
+
+    fig.tight_layout()
+
+    return fig, axes
+
+
+def create_feature_importance_plot(pipeline: Pipeline, **kwargs):
+    """
+    kwargs: figsize
+    """
+    figsize = kwargs.get("figsize", (18, 6))
+
+    model = pipeline.named_steps["model"]
+    feature_importances = model.feature_importances_
+    feature_importances /= feature_importances.sum()
+    df = pd.DataFrame({
+        "feature_name": pipeline.named_steps["data_preprocessing"].get_feature_names_out(),
+        "feature_importance": feature_importances
+    })
+
+    fig, axis = plt.subplots(figsize=figsize)
+    df.plot(kind="bar", x="feature_name", y="feature_importance", rot=90, ax=axis, ylabel="Relative importance")
+
+    return fig, axis
---
+++ models/model_selection.py
@@ -0,0 +1,222 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+import sys
+import os
+import pickle
+
+from scipy.stats.mstats import winsorize
+from sklearn.pipeline import Pipeline
+from sklearn.compose import make_column_selector, ColumnTransformer
+from sklearn.preprocessing import StandardScaler, FunctionTransformer, OneHotEncoder
+from skopt import BayesSearchCV
+from sklearn.metrics import r2_score, root_mean_squared_error
+from typing import List, Union
+from sklearn.linear_model import Ridge
+from regressors import stats
+
+
+np.int = int
+
+ROOT = os.path.abspath(__file__)
+for _ in range(2):
+    ROOT = os.path.dirname(ROOT)
+
+PATH = os.path.dirname(ROOT)
+if PATH not in sys.path:
+    sys.path.append(PATH)
+
+
+from BusinessAnalyticsProject.configs import load_global_config, add_prefix_to_feature_names
+
+GlOBAL_CONFIG = load_global_config()
+
+
+def identity(x):
+    return x
+
+
+def winsorization(df: pd.DataFrame, quantile_cut=0.02, if_plot=False):
+    df_out = df.apply(lambda x: winsorize(x, limits=(quantile_cut, quantile_cut)).data, axis=0)
+
+    if if_plot:
+        fig, axis = plt.subplots()
+        (df_out != df).mean().plot(kind="bar", ax=axis)
+
+        return df_out, fig, axis
+
+    return df_out
+
+
+def create_pipeline(model_cls, param_dict, if_winsorization=False, if_data_normalization=False,
+                    if_to_one_hot=False) -> Pipeline:
+    """
+    Parameters
+    ----------
+    model_cls: a model class (not an instance) or None
+    param_dict: dict or None
+        Parameters to initialize a model_cls
+    categorical_cols: list of str
+        Categorical columns
+    if_winsorization: bool
+        Whether to do winsorization
+    if_data_normalization: bool
+        Whether to do mean-std normalization
+    if_to_one_hot: bool
+        Whether to do one-hot coding for categorical features
+
+    Returns
+    -------
+    Pipeline
+    """
+    cate_cols_selector = make_column_selector(dtype_include="category")
+    cont_cols_selector = make_column_selector(dtype_exclude="category")
+
+    cate_cols_steps = []
+    if if_to_one_hot:
+        cate_cols_steps.append(("one_hot", OneHotEncoder()))
+    else:
+        cate_cols_steps.append(("dummy", FunctionTransformer(identity, feature_names_out="one-to-one")))
+    pipeline_cate = Pipeline(cate_cols_steps)
+
+    cont_cols_steps = []
+    if if_winsorization:
+        cont_cols_steps.append(("winsorization", FunctionTransformer(winsorization, feature_names_out="one-to-one")))
+    if if_data_normalization:
+        cont_cols_steps.append(("standard_norm", StandardScaler()))
+    if len(cont_cols_steps) == 0:
+        cont_cols_steps.append(("dummy", FunctionTransformer(identity, feature_names_out="one-to-one")))
+    pipeline_cont = Pipeline(cont_cols_steps)
+
+    col_transformer = ColumnTransformer(
+        [
+            ("categorical", pipeline_cate, cate_cols_selector),
+            ("continuous", pipeline_cont, cont_cols_selector)
+        ]
+    )
+
+    if model_cls is None and param_dict is None:
+        return col_transformer
+
+    model = model_cls(**param_dict)
+    pipeline_out = Pipeline([
+        ("data_preprocessing", col_transformer),
+        ("model", model)
+    ])
+
+    return pipeline_out
+
+
+def hyperparam_tuning(
+        pipeline: Pipeline,
+        param_space: dict,
+        X_train: pd.DataFrame,
+        y_train: pd.DataFrame,
+        weights_train: pd.Series,
+        X_test: Union[pd.DataFrame, None] = None,
+        y_test: Union[pd.Series, None] = None,
+        weights_test: Union[pd.Series, None] = None,
+        save_dir=None):
+    model_step_name = list(pipeline.named_steps.keys())[-1]
+    param_space_with_prefix = add_prefix_to_feature_names(model_step_name, param_space)
+    cv_params = GlOBAL_CONFIG["cv_params"]
+    opt = BayesSearchCV(pipeline, param_space_with_prefix,
+                        fit_params={f"{model_step_name}__sample_weight": weights_train}, **cv_params)
+    opt.fit(X_train, y_train)
+
+    metrics_out = None
+    if X_test is not None and y_test is not None and weights_test is not None:
+        metrics_out = metrics(opt, X_test, y_test, weights_test)
+
+    if save_dir is not None:
+        if not os.path.isdir(save_dir):
+            os.makedirs((save_dir))
+        df_cv_results = pd.DataFrame(opt.cv_results_)
+        df_cv_results.to_csv(os.path.join(save_dir, "cv_results.csv"))
+        with open(os.path.join(save_dir, "cv_model_and_metrics.pkl"), "wb") as wf:
+            pickle.dump({
+                "model": opt,
+                "metrics": metrics_out
+            }, wf)
+
+    return opt, metrics_out
+
+
+def metrics(pipeline: Pipeline, X: pd.DataFrame, y: pd.Series, weights: pd.Series):
+    y = y.values
+    y_pred = pipeline.predict(X)
+    out_dict = {
+        "r2_score": r2_score(y, y_pred),
+        "rmse": root_mean_squared_error(y, y_pred),
+    }
+    out_dict["rwmse"] = (weights @ (y - y_pred) ** 2).mean() ** 0.5
+
+    return out_dict
+
+
+def benchmark(
+        pipeline_data: Union[Pipeline, None],
+        X_train: pd.DataFrame, y_train: pd.Series, weights_train: pd.Series,
+        X_test: pd.DataFrame, y_test: pd.Series, weights_test: pd.Series
+):
+    if pipeline_data is not None:
+        X_train = pipeline_data.fit_transform(X_train)
+        X_test = pipeline_data.transform(X_test)
+        X_train = pd.DataFrame(X_train.toarray(), columns=pipeline_data.get_feature_names_out(), index=y_train.index)
+        X_test = pd.DataFrame(X_test.toarray(), columns=pipeline_data.get_feature_names_out(), index=y_test.index)
+
+    X_train = sm.add_constant(X_train)
+    X_test = sm.add_constant(X_test)
+    model = sm.WLS(y_train, X_train, weights=weights_train ** 2)  # Input W -> Pre-multiply by sqrt(W)
+    results = model.fit()
+    metrics_val = metrics(results, X_test, y_test, weights_test)
+
+    return results, metrics_val
+
+def OLS(
+    pipeline_data: Union[Pipeline, None],
+        X_train: pd.DataFrame, y_train: pd.Series, weights_train: pd.Series,
+        X_test: pd.DataFrame, y_test: pd.Series, weights_test: pd.Series
+):
+    if pipeline_data is not None:
+        X_train = pipeline_data.fit_transform(X_train)
+        X_test = pipeline_data.transform(X_test)
+        X_train = pd.DataFrame(X_train.toarray(), columns=pipeline_data.get_feature_names_out(), index=y_train.index)
+        X_test = pd.DataFrame(X_test.toarray(), columns=pipeline_data.get_feature_names_out(), index=y_test.index)
+
+    X_train = sm.add_constant(X_train)
+    X_test = sm.add_constant(X_test)
+    results = sm.OLS(y_train, X_train).fit()
+    metrics_val = metrics(results, X_test, y_test, weights_test)
+    
+    ols_resid = results.resid
+    res_fit = sm.OLS(ols_resid[1:].tolist(), ols_resid[:-1].tolist()).fit()
+    rho = res_fit.params # for GLS
+
+    return results, metrics_val, rho
+
+def RR(
+    pipeline_data: Union[Pipeline, None],
+        X_train: pd.DataFrame, y_train: pd.Series, weights_train: pd.Series,
+        X_test: pd.DataFrame, y_test: pd.Series, weights_test: pd.Series
+):
+    if pipeline_data is not None:
+        X_train = pipeline_data.fit_transform(X_train)
+        X_test = pipeline_data.transform(X_test)
+        X_train = pd.DataFrame(X_train.toarray(), columns=pipeline_data.get_feature_names_out(), index=y_train.index)
+        X_test = pd.DataFrame(X_test.toarray(), columns=pipeline_data.get_feature_names_out(), index=y_test.index)
+
+    X_train = sm.add_constant(X_train)
+    X_test = sm.add_constant(X_test)
+    model = Ridge()
+    results = model.fit(X_train, y_train)
+    metrics_val = metrics(results, X_test, y_test, weights_test)
+    
+    pval = stats.coef_pval(results, X_train, y_train)
+    summary = stats.summary(results, X_train, y_train)
+    
+    return results, metrics_val,pval, summary
+    
+
