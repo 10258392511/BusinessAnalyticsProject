33446c9eaccd2785680324375d13541011b4f809
Xinran Cheng <121287133+Cheshire4@users.noreply.github.com>
2024-03-29 15:58:57 +0100

    Update LSO.py

--- configs/LSO.py
+++ configs/LSO.py
@@ -6,12 +6,11 @@ from sklearn.linear_model import Lasso
 CONFIG = {
     "model_cls": Lasso,
     "params": {
-        "alpha": 1e-3,
-        "tol": 1e-4
+        "alpha": 1e-10,
     },
     "param_space": {
-        "alpha": [1e-10, 1e-8, 1e-6, 1e-5,1e-4],
-        "tol": [1e-3,5e-3,1e-2,5e-2,1e-1,5e-1,1,5,10]
+        "alpha": [1e-10, 1e-8, 1e-6, 1e-5,1e-4,1e-3,1e-2],
         
     }
 }
+
