89765ccc65d1e7ee7c9ab1586d5972aea30204b0
Xinran Cheng <121287133+Cheshire4@users.noreply.github.com>
2024-03-29 15:58:27 +0100

    Update RR.py

--- configs/RR.py
+++ configs/RR.py
@@ -7,12 +7,10 @@ CONFIG = {
     "model_cls": Ridge,
     "params": {
         "alpha": 1,
-        # "fit_intercept": 1,
-        "tol": 1e-4
     },
+
     "param_space": {
         "alpha": [1e-15, 1e-12, 1e-10,1e-5, 0.001, 0.01, 0.05, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5, 1],
-        # "fit_intercept": [0,1],
-        "tol": [1e-4, 1e-5, 1e-3]
     }
 }
+
